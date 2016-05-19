69ab81ce59e662cfec79a04e404c1f9432ff2c86
Eric Yuan <ksyuan@icloud.com>
2016-05-19 17:15:41 +0800

    reg ex

--- Adventure/MyPlayground.playground/Contents.swift
+++ Adventure/MyPlayground.playground/Contents.swift
@@ -1,5 +1,21 @@
 import Foundation
 
+extension String {
+    /// EZSE: Cut string from integerIndex to the end
+    public subscript(integerIndex: Int) -> Character {
+        let index = startIndex.advancedBy(integerIndex)
+        return self[index]
+    }
+    
+    /// EZSE: Cut string from range
+    public subscript(integerRange: Range<Int>) -> String {
+        let start = startIndex.advancedBy(integerRange.startIndex)
+        let end = startIndex.advancedBy(integerRange.endIndex)
+        let range = start..<end
+        return self[range]
+    }
+}
+
 private func check(str: String) {
     // 使用正则表达式一定要加try语句
     do {
@@ -12,8 +28,10 @@ private func check(str: String) {
         let res = regex.matchesInString(str, options: NSMatchingOptions(rawValue: 0), range: NSMakeRange(0, str.characters.count))
         // 输出结果
         for checkingRes in res {
-            debugPrint(checkingRes)
-            print((str as NSString).substringWithRange(checkingRes.range))
+            let color = str[checkingRes.rangeAtIndex(1).toRange()!]
+            print("color is \(color)")
+            let content = str[checkingRes.rangeAtIndex(2).toRange()!]
+            print("content is \(content)")
         }
     }
     catch {
